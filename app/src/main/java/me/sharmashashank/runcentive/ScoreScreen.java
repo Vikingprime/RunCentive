2c5890f2956a0fc51e74f3f9c0a071adbb4837af
Vikas <vikas.s.pydah@vanderbilt.edu>
2015-10-03 16:14:49 -0500

    ScoreScreen

---
+++ app/src/main/java/me/sharmashashank/runcentive/ScoreScreen.java
@@ -0,0 +1,19 @@
+package me.sharmashashank.runcentive;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScoreScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_screen);
+    }
+
+}
