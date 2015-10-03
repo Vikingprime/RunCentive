1ef3c9aebaf323a7f1027947c9955c32c37af750
shashank_sharma_ <8shashank@gmail.com>
2015-10-03 02:25:02 -0500

    Small changes to API wrapper

--- app/src/main/java/me/sharmashashank/runcentive/NessieWrapper.java
+++ app/src/main/java/me/sharmashashank/runcentive/NessieWrapper.java
@@ -13,6 +13,12 @@ import java.util.ArrayList;
 public class NessieWrapper {
     NessieClient nessieClient;
     private static final String TAG = NessieWrapper.class.getName();
+    private static final String testCustomer="560f0205f8d8770df0ef9b3b";
+    private static final String testSavingAccount="560f0206f8d8770df0efa2af";
+    private static final String testCheckingAccount="560f0206f8d8770df0efa2b0";
+
+    private Account savingsAccount;
+    private Account checkingAccount;
 
     public NessieWrapper(String apiKey){
         nessieClient=NessieClient.getInstance();
@@ -22,17 +28,25 @@ public class NessieWrapper {
     }
 
     public void getAccounts(){
-        nessieClient.getAccounts(new NessieResultsListener() {
+        nessieClient.getCustomerAccounts(testCustomer, new NessieResultsListener() {
             @Override
             public void onSuccess(Object o, NessieException e) {
-                if (e==null) {
-                    ArrayList<Account> accounts = (ArrayList<Account>) o;
-                    Log.d(TAG,accounts.get(0).toString());
+                if (e==null){
+                    ArrayList<Account> accounts=(ArrayList<Account>) o;
+                    for(Account acc: accounts){
+                        if (acc.get_id().equals(testSavingAccount)){
+                            savingsAccount=acc;
+                        }
+                        else if (acc.get_id().equals(testCheckingAccount)){
+                            checkingAccount=acc;
+                        }
+                    }
                 }
                 else{
-                    Log.d(TAG, "Cannot get all accounts" + e.getMessage());
+                    Log.d(TAG, "Did not get accounts");
                 }
             }
         });
     }
+
 }
